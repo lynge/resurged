4f1147fc867df94b5d4f7b35a1f1d8b4ba3b7a08
Brian Pedersen <brian.pedersen@gmail.com>
2010-02-14 02:18:40 +0100

    should of course be abstract

--- src/test/java/junit/AbstractTestCase.java
+++ src/test/java/junit/AbstractTestCase.java
@@ -2,7 +2,7 @@ package junit;
 import junit.framework.TestCase;
 import org.resurged.Log;
 
-public class AbstractTestCase extends TestCase {
+public abstract class AbstractTestCase extends TestCase {
 	public AbstractTestCase(){
 		Log.setLogger(new Log.ConsoleLogger());
 	}
